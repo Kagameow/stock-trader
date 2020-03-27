71bb5a7b5c52252b70c1705f020542cbb64e760d
Kagameow <kagami.404@gmail.com>
2020-03-27 22:15:00 +0300

    Simple rules for form validation

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -15,34 +15,51 @@
                     <v-icon large>mdi-plus</v-icon>
                 </v-tab>
                 <v-tab-item>
-                    <v-form class="px-4 mt-3">
+                    <v-form class="px-4 mt-3" v-model="isFormValid">
                         <v-text-field label="E-mail"
-                                      prepend-icon="mdi-account-box"
+                                      prepend-icon="mdi-email"
+                                      :rules="rules.email"
                                       required
                         >
                         </v-text-field>
                         <v-text-field label="Password"
                                       prepend-icon="mdi-lock"
+                                      type="password"
+                                      :rules="rules.password"
                                       required>
                         </v-text-field>
                         <v-card-actions class="justify-center">
-                            <v-btn class="mb-2" x-large @click="true">Login</v-btn>
+                            <v-btn class="mb-2"
+                                   large
+                                   @click="true"
+                                   :disabled="!isFormValid"
+                            >Login
+                            </v-btn>
                         </v-card-actions>
                     </v-form>
                 </v-tab-item>
                 <v-tab-item>
-                    <v-form class="px-4 mt-3">
+                    <v-form class="px-4 mt-3" v-model="isFormValid">
                         <v-text-field label="E-mail"
-                                      prepend-icon="mdi-account-box"
+                                      prepend-icon="mdi-email"
+                                      :rules="rules.email"
                                       required
                         >
                         </v-text-field>
                         <v-text-field label="Password"
                                       prepend-icon="mdi-lock"
+                                      type="password"
+                                      :rules="rules.password"
                                       required>
                         </v-text-field>
                         <v-card-actions class="justify-center">
-                            <v-btn class="mb-2" x-large @click="true">Sign Up</v-btn>
+                            <v-btn class="mb-2"
+                                   large
+                                   :disabled="!isFormValid"
+                                   @click="true"
+                            >
+                                Sign Up
+                            </v-btn>
                         </v-card-actions>
                     </v-form>
                 </v-tab-item>
@@ -53,7 +70,22 @@
 
 <script>
     export default {
-        name: "Authentication"
+        name: "Authentication",
+        data() {
+            return {
+                rules: {
+                    email: [
+                        v => !!v || 'E-mail is required',
+                        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                    ],
+                    password: [
+                        v => !!v || 'Password is required',
+                        v => (v && v.length >= 8) || 'Password must be at least 8 characters long',
+                    ]
+                },
+                isFormValid: true
+            }
+        },
     }
 </script>
 
