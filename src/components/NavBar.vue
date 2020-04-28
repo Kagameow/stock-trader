c0adf30570b33b01063783a70aa627a08af51f90
Kagameow <kagami.404@gmail.com>
2020-04-28 23:37:58 +0300

    Now UI is adaptive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,42 @@
 <template>
     <v-app-bar app>
-        <v-toolbar-title>
-            <v-btn text to="/">
-                <h3>Stocks Trader</h3>
+            <v-btn text to="/" :x-small="isMobile">
+                <h3 class="d-md-none">
+                   {{funds}}$
+                </h3>
+                <h3 class="d-none d-md-block">Stocks Trader</h3>
             </v-btn>
-        </v-toolbar-title>
-        <v-btn text to="/portfolio">
-            Portfolio
+        <v-btn text to="/portfolio" :x-small="isMobile">
+            <span>
+                <v-icon>mdi-wallet-outline</v-icon>
+            </span>
+            <span class="d-none d-md-block">
+                Portfolio
+            </span>
         </v-btn>
-        <v-btn text to="/stocks">
-            Stocks
+        <v-btn text to="/stocks" :x-small="isMobile">
+            <span>
+            <v-icon>mdi-cart-plus</v-icon>
+            </span>
+            <span class="d-none d-md-block">
+                Stocks
+            </span>
         </v-btn>
         <v-spacer></v-spacer>
         <v-menu offset-y>
             <template v-slot:activator="{ on }">
                 <v-btn v-on="on"
                        :disabled="!isAuthenticated"
-                       text>
-                    Save & Load
-                    <v-icon>mdi-menu-down</v-icon>
+                       text
+                       :x-small="isMobile"
+                >
+                    <span>
+                    <v-icon>mdi-content-save-all-outline</v-icon>
+                    </span>
+                    <span class="d-none d-md-block">
+                        Save & Load
+                        <v-icon>mdi-menu-down</v-icon>
+                    </span>
                 </v-btn>
             </template>
             <v-list>
@@ -30,23 +48,38 @@
                 </v-list-item>
             </v-list>
         </v-menu>
-        <v-btn class="mr-2" text @click="newDayCalculation">
+        <v-btn text @click="newDayCalculation" :x-small="isMobile">
+            <span>
+            <v-icon>mdi-bed-outline</v-icon>
+            </span>
+            <span class="d-none d-md-block">
             End Day
+            </span>
         </v-btn>
-        <v-btn class="mr-2"
-               v-if="isAuthenticated"
+        <v-btn v-if="isAuthenticated"
                @click="logout"
-               text>
-            Logout
-        </v-btn>
-        <v-btn class="mr-2"
-               v-else
                text
-               to="/login">
-            Login
+               :x-small="isMobile">
+            <span>
+                <v-icon>mdi-logout</v-icon>
+            </span>
+            <span class="d-none d-md-block">
+                Logout
+            </span>
         </v-btn>
-        <span class="font-weight-black">
-            Funds: {{funds}}$
+        <v-btn v-else
+               text
+               :x-small="isMobile"
+               to="/login">
+            <span>
+                <v-icon>mdi-login</v-icon>
+            </span>
+            <span class="d-none d-md-block">
+            Login
+            </span>
+        </v-btn>
+        <span class="font-weight-black d-none d-md-block">
+            Funds: {{ funds}}$
         </span>
     </v-app-bar>
 </template>
@@ -63,6 +96,9 @@
             isAuthenticated() {
                 return this.$store.getters.isAuthenticated;
             },
+            isMobile() {
+                return this.$vuetify.breakpoint.xsOnly
+            }
         },
         methods: {
             ...mapActions([
